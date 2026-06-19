f6ed94c8de421c4136341f3a554d61aeee1d12a8
Yulia Karaliyska <y.karaliyska@hotmail.com>
2026-06-19 17:00:12 +0300

    some changes

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ cloudinary.config({
 
 //this is for uploading avatars
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "client", "dist")));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -41,6 +41,12 @@ app.use(express.json());
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
+
+
+//for build client
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
