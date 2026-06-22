70e5808fdda6578f08f8ecd5ae593d0274d26142
Yulia Karaliyska <y.karaliyska@hotmail.com>
2026-06-22 15:05:04 +0300

    fixed update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
 import { StatusCodes } from "http-status-codes";
+import { formatImage } from "../middleware/multerMiddleware.js";
 import Job from "../models/JobModel.js";
 import User from "../models/UserModel.js";
 
@@ -21,8 +21,9 @@ export const updateUser = async (req, res) => {
   delete newUser.password;
 
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
+
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
